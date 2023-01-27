36b5905d26edcfc638af06aa8cef9b9f5e42fa09
Mikita Klimenkou <EternityXDX@gmail.com>
2023-01-27 18:26:17 +0300

    Solve Tasks

---
+++ HomeWorks/HomeWork3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SequenceOfNumbers();
+            DoubleDigit();
+            Average();
+        }
+
+        private static void SequenceOfNumbers()
+        {
+            int number = -20;
+            
+            do
+            {
+                if (number % 20 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+
+                number--;
+            } while (number >= -100);
+        }
+
+        private static void DoubleDigit()
+        {
+            for (int i = 10; i < 100; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+
+        private static void Average()
+        {
+            int summ = 0;
+            int count = 0;
+            int average = 0;
+            
+            for (int i = 1; i <= 100; i++)
+            {
+                count++;
+                summ += i;
+            }
+
+            average = summ / count;
+            
+            Console.WriteLine($"Среднее арифметическое равно: {average} \nСумма всех целых чисел равна: {summ}");
+        }
+    }
+}
